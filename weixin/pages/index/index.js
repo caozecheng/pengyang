577487e107323ae3d9ad8186ccdf6714f7720bfc
caozc <caozc@hanwintech.com>
2020-12-01 16:49:16 +0800

    轮播图有问题

--- weixin/pages/index/index.js
+++ weixin/pages/index/index.js
@@ -3,62 +3,53 @@
 const app = getApp()
 
 Page({
-  clickMe: function() {
-    this.setData({ msg: "Hello World" })
-    this.isShow="false"
-  },
   data: {
-    isShow:"true",
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    focus: false,
+    inputValue: '',
+    movies:[  
+      {url:'../../images/swer1.png'} ,  
+      {url:'../../images/swer2.png'} ,  
+      {url:'../../images/swer3.png'} ,  
+      {url:'../../images/swer4.png'},  
+      {url:'../../images/swer5.png'},   
+      ] 
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
+  onShareAppMessage() {
+    return {
+      title: 'swiper',
+      path: 'page/component/pages/swiper/swiper'
     }
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
+  bindKeyInput: function (e) {
     this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+      inputValue: e.detail.value
     })
   },
-  logBtn:function(options){
-    wx.switchTab({
-      url: '../ivew/pages1/pages1',
-    })
-}
+  bindReplaceInput: function (e) {
+    var value = e.detail.value
+    var pos = e.detail.cursor
+    var left
+    if (pos !== -1) {
+      // 光标在中间
+      left = e.detail.value.slice(0, pos)
+      // 计算光标的位置
+      pos = left.replace(/11/g, '2').length
+    }
+
+    // 直接返回对象，可以对输入进行过滤处理，同时可以控制光标的位置
+    return {
+      value: value.replace(/11/g, '2'),
+      cursor: pos
+    }
+
+    // 或者直接返回字符串,光标在最后边
+    // return value.replace(/11/g,'2'),
+  },
+  bindHideKeyboard: function (e) {
+    if (e.detail.value === '123') {
+      // 收起键盘
+      wx.hideKeyboard()
+    }
+  },
+
 })
